a538980fda5c58df0de375f7875f9e80c9855495
Gingerjanitor <Manierre@udel.edu>
2023-08-05 12:42:39 -0400

    graph ouput added
    Added a nifty graph

--- Inflation adjustment calculator.py
+++ Inflation adjustment calculator.py
@@ -8,9 +8,10 @@ Created on Thu Aug  3 22:26:27 2023
 import pandas as pd
 import requests
 import numpy as np
-import matplotlib.pyplot as plt
+import seaborn as sns
 import datetime 
 from fredapi import Fred
+import matplotlib.pyplot as plt
 
 def takevalue(pay):
     pay=pay.replace('$',"")
@@ -38,26 +39,63 @@ monthprior=today-datetime.timedelta(weeks=16)
 
 ###########make the query
 
-fred = Fred(api_key='APIGOESHERE')
+fred = Fred(api_key='KEY')
 initialinflation = fred.get_series('CPIAUCNS',startingdate,startingdateend)
 currentinflation = fred.get_series('CPIAUCNS',monthprior,today)
 
 ##calculate changes
+###This pulls together inflation 3 months prior to the date and for january of the provided year.
+
 inflationadj=(startingpay*(currentinflation.mean()/initialinflation.mean())).round(2)
 delta=(endingpay-inflationadj).round(2)
 deltapct=(((endingpay/inflationadj)-1)*100).round(2)
 
 print(f"""\n\nBack in {startingyear} you earned ${startingpay}. If we converted that into {datetime.date.today().year} dollars, you would be making ${inflationadj} dollars! How's the {endingpay} that you're earning look now?""")
 
+###Throw them in DFs
+
+dates=pd.to_datetime(pd.Series([startingdate, todayformat],name="dates"))
+adjusted=pd.Series([startingpay,inflationadj], name="adjusted").astype(int)
+unadjusted=pd.Series([startingpay, endingpay],name="unadjusted").astype(int)
+df=pd.concat([adjusted,unadjusted],axis=1).set_index(dates)
+pd.concat([dates,df],axis=1)
+
+
+
+
+#tailor msg based on the decline
 if delta<0:
     print(f"\n Compared to when you were hired, you've taken what amounts to a ${delta} or %{deltapct} pay cut.")
 
 elif delta>0:
     print(f"\n Compared to when you were hired, you've actually gotten a raise! Yay! Compared to when you were hired,you're making ${delta} or %{deltapct} more than when you were hired. Still, think how many years of experience you've got- I hope it's commensurate!")
 
+sns.set_theme()
 
 
-###This pulls together inflation 3 months prior to the date and for january of the provided year.
+plt.figure(figsize=(10, 6))
+
+line1=sns.lineplot(df,x=df.index,y=adjusted, label="What your starting pay is now worth")
+line2=sns.lineplot(df,x=df.index,y=unadjusted, label="What you're currently being paid")
+
+
+line1.set_ylabel("pay")
+for x, y in zip(df.index, df['unadjusted']):
+ # the position of the data label relative to the data point can be adjusted by adding/subtracting a value from the x &/ y coordinates
+ plt.text(x = x, # x-coordinate position of data label
+ y = y-800, # y-coordinate position of data label, adjusted to be 150 below the data point
+ s = '${:.0f}'.format(y), # data label, formatted to ignore decimals
+ color = 'black') # set colour of line
+
+
+for x, y in zip(df.index, df['adjusted']):
+ # the position of the data label relative to the data point can be adjusted by adding/subtracting a value from the x &/ y coordinates
+ plt.text(x = x, # x-coordinate position of data label
+ y = y-800, # y-coordinate position of data label, adjusted to be 150 below the data point
+ s = '${:.0f}'.format(y), # data label, formatted to ignore decimals
+ color = 'black') # set colour of line
+
+plt.show()
 
 
 
